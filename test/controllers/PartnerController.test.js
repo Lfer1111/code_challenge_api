dd2ed719561e9ad3c8370e187e97820efbddb24c
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-05-02 01:07:49 -0500

    Paso 14: Usando ESLinter para arreglar el codigo

--- test/controllers/PartnerController.test.js
+++ test/controllers/PartnerController.test.js
@@ -1,17 +1,17 @@
-const PartnerController = require("../../lib/controllers/PartnerController")
+const PartnerController = require("../../lib/controllers/PartnerController");
 
 describe("Test para PartnerController", () => {
     test("Test 1. Probar el primer metodo, total de estudiantes", () => {
-        const totalStudents = PartnerController.getAllStudents()
-        expect(totalStudents.length).toBe(51)
-    })
+        const totalStudents = PartnerController.getAllStudents();
+        expect(totalStudents.length).toBe(51);
+    });
     test("Test 2. Emails de estudiantes con Certificado", () => {
-        const emailsStudents = PartnerController.getEmailStudentsCertificated()
-        expect(emailsStudents).toContain("Todd@visualpartnership.xyz")
-    })
+        const emailsStudents = PartnerController.getEmailStudentsCertificated();
+        expect(emailsStudents).toContain("Todd@visualpartnership.xyz");
+    });
     test("Test 3. Estudiantes con creditos mayores a 500", () => {
-        const studentsHighCredit = PartnerController.getHighCreditsStudents(500)
-        expect(studentsHighCredit.length).toBeGreaterThanOrEqual(27)
-    })
+        const studentsHighCredit = PartnerController.getHighCreditsStudents(500);
+        expect(studentsHighCredit.length).toBeGreaterThanOrEqual(27);
+    });
 
-})
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,14 +1,14 @@
-const Reader = require("../../lib/utils/Reader")
+const Reader = require("../../lib/utils/Reader");
 
 describe("Test para el Reader", () => {
     test("Test 1. Probando el Reader con todos los estudiantes", () => {
-        const students = Reader.readJsonFile("visualpartners.json")
-        expect(students.length).toBe(51)
-    })
+        const students = Reader.readJsonFile("visualpartners.json");
+        expect(students.length).toBe(51);
+    });
     test("Test 2. Cantidad de estudiantes con Certificado", () => {
-        const students = Reader.readJsonFile("visualpartners.json")
-        const studentsCertificated = students.filter((student) => student.haveCertification === true)
-        expect(studentsCertificated.length).toBe(29)
-    })
+        const students = Reader.readJsonFile("visualpartners.json");
+        const studentsCertificated = students.filter((student) => student.haveCertification === true);
+        expect(studentsCertificated.length).toBe(29);
+    });
 
-})
+});
